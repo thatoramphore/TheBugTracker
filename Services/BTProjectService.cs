1d6374e94b4e4135c1c0c20521275d6c9dda2bfa
thatoramphore <r2.thato@gmail.com>
2022-07-16 23:14:35 +0200

    Update Project Feature

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -204,11 +204,24 @@ namespace TheBugTracker.Services
         //CRUD - Read
         public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
         {
-            Project project = await _context.Projects
+            /*Project project = await _context.Projects
                                                     .Include(p => p.Tickets)
                                                     .Include(p => p.Members)
                                                     .Include(p => p.ProjectPriority)
-                                                    .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
+                                                    .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);*/
+
+            Project project = await _context.Projects
+                                                    .Include(p => p.Tickets)
+                                                        .ThenInclude(t => t.TicketPriority)
+                                                    .Include(p => p.Tickets)
+                                                        .ThenInclude(t => t.TicketStatus)
+                                                    .Include(p => p.Tickets)
+                                                        .ThenInclude(t => t.DeveloperUser)
+                                                    .Include(p => p.Tickets)
+                                                        .ThenInclude(t => t.OwnerUser)
+                                                    .Include(p => p.Members)
+                                                    .Include(p => p.ProjectPriority)
+                                                    .FirstOrDefaultAsync(p =>p.Id == projectId && p.CompanyId == companyId);
             return project;
         }
 
