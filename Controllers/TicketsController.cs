6d1e6410d22aafa7ddbc87f418c7df30e278fe14
thatoramphore <r2.thato@gmail.com>
2022-07-25 00:52:51 +0200

    Update Ticket Features

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -113,15 +113,29 @@ namespace TheBugTracker.Controllers
             }
         }
 
+        //GET: Tickets/AssignDeveloper
         [HttpGet]
         public async Task<IActionResult> AssignDeveloper(int id)
         {
             AssignDeveloperViewModel model = new();
 
             model.Ticket = await _ticketService.GetTicketByIdAsync(id);
-            model.Developers = new SelectList(await _projectService.GetProjectMembersByRoleAsync(model.Ticket.Id, nameof(Roles.Developer)), "Id", "FullName");
+            model.Developers = new SelectList(await _projectService.GetProjectMembersByRoleAsync(model.Ticket.ProjectId, nameof(Roles.Developer)), "Id", "FullName");
 
             return View(model);
+        }
+
+        //POST: Tickets/AssignDeveloper
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignDeveloper(AssignDeveloperViewModel model)
+        {
+            if(model.DeveloperId != null)
+            {
+                await _ticketService.AssignTicketAsync(model.Ticket.Id, model.DeveloperId);
+            }
+
+            return RedirectToAction(nameof(AssignDeveloper), new { id = model.Ticket.Id });
         }
 
         // GET: Tickets/Details/5
